c6b895184886235fd12e3122f148e85fd04e6e2a
manoj <manoj@essential.com>
2019-05-23 18:07:37 +0530

    Introducing crash recovery - partial

--- app/src/main/java/com/manoj/smartreply/SmartReplyUtil.kt
+++ app/src/main/java/com/manoj/smartreply/SmartReplyUtil.kt
@@ -19,16 +19,21 @@ import java.io.*
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
+import android.content.Context.MODE_PRIVATE
+import android.R.id.edit
+import android.content.SharedPreferences
+
+
 
 class SmartReplyUtil(handler: Handler?) {
+
 
     private var inputStream: InputStream? = null
 
     private var smartReply: FirebaseSmartReply? = null
     private var bufferedWriter: BufferedWriter? = null
 //    private var currentLine:String? = null
-    private var taskCount = 0
-    private var completedTaskCount = 0
+    private var completedTaskCount = 0L
 
     private var handler = handler
 
@@ -39,7 +44,7 @@ class SmartReplyUtil(handler: Handler?) {
     public var appContext:Context? = null
     public var fileUri: Uri? = null
 
-    private var threadPoolExecutor:ThreadPoolExecutor? = null
+//    private var threadPoolExecutor:ThreadPoolExecutor? = null
 
     val taskHandler: Handler = object : Handler() {
         override fun handleMessage(msg: Message) {
@@ -85,102 +90,62 @@ class SmartReplyUtil(handler: Handler?) {
         inputStream = appContext!!.contentResolver.openInputStream(fileUri!!)
         fileReader = BufferedReader(InputStreamReader(inputStream))
 
+        var filename = "smart_reply_output_" + System.currentTimeMillis() + ".csv"
+        completedTaskCount = 0
+
+
+
+        Utils.updatePrefWithFilename(appContext!!, filename)
+
         if (smartReply == null) {
             smartReply = FirebaseNaturalLanguage.getInstance().smartReply
             val file = File(
                 Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_DOWNLOADS), "smart_reply_output.csv")
+                    Environment.DIRECTORY_DOWNLOADS
+                ), filename
+            )
             bufferedWriter = BufferedWriter(FileWriter(file.path))
         }
 
 
-        completedTaskCount = 0
-        taskCount = 0
         startTime = System.currentTimeMillis()
 
-//        threadPoolExecutor = ThreadPoolExecutor(16, 32, 10,
-        threadPoolExecutor = ThreadPoolExecutor(1, 128, 0,
-            TimeUnit.MILLISECONDS, LinkedBlockingQueue<Runnable>())
-        var line: String? = fileReader!!.readLine()
-        while(line != null) {
-            taskCount = taskCount + 1
-            threadPoolExecutor!!.execute(
-                SmartReplyRunnable(line!!, taskCount, handler, taskHandler, bufferedWriter, smartReply!!)
-            )
-            line = fileReader!!.readLine()
-        }
 
-        Handler().postDelayed({
-            checkStatus()
-        }, 2)
+        Log.d("MM", "FileName: " + Utils.getFilenameFromPref(appContext!!))
+        Log.d("MM", "LLP: " + Utils.getLLPFromPref(appContext!!))
+
+        var lastProcessedLine = Utils.getLLPFromPref(appContext!!)
+        moveToLine(lastProcessedLine)
+        process()
     }
 
-    fun checkStatus() {
-        if (threadPoolExecutor == null) {
+    fun moveToLine(lineNo:Long): Boolean {
+        while (completedTaskCount < lineNo) {
+            var line = getNextLine()
+            if (line == null) {
+                return false
+            }
+            completedTaskCount = completedTaskCount + 1
+        }
+
+        return true
+    }
+
+    fun process() {
+        var line = getNextLine()
+
+        if (line == null) {
+            Handler().postDelayed({
+                checkStatus()
+            }, 2)
             return
         }
 
-        if (completedTaskCount == taskCount) {
-
-            endTime = System.currentTimeMillis()
-            val diff = (endTime - startTime)/(1000)
-
-            threadPoolExecutor = null
-            updateUI("Done, total $completedTaskCount records processed\nOutput: Downloads/smart_reply_output.csv\nTime: $diff(s)")
-            bufferedWriter!!.close()
-            bufferedWriter = null
-            inputStream!!.close()
-            inputStream = null
-            fileReader!!.close()
-            fileReader = null
-            smartReply!!.close()
-            smartReply = null
-            val scheduler = appContext!!.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
-            scheduler.cancel(SmartReplyJobService.JOB_ID)
-
-        } else {
-            updateUI("Total $completedTaskCount/$taskCount records processed")
-
-            Handler().postDelayed({
-                checkStatus()
-            }, 100)
-        }
-    }
-}
-
-
-// Implementing the Runnable interface to implement threads.
-class SmartReplyRunnable(lineP: String, countP: Int, handlerP: Handler?, taskCompletionHandlerP: Handler,
-                         bufferedWriterP: BufferedWriter?, smartReplyP: FirebaseSmartReply): Runnable {
-
-    private var handler = handlerP
-    private var taskCompletionHandler = taskCompletionHandlerP
-    private var bufferedWriter = bufferedWriterP
-    private var line = lineP
-    private var count = countP
-    private var smartReply: FirebaseSmartReply = smartReplyP
-
-    public override fun run() {
-
-        processLine(line)
-
+        processLine(line!!, completedTaskCount+1)
     }
 
-    fun processLine(line:String)  {
-//        if (line == null) {
-//            updateUI("Done, total $count records processed\nOutput: Downloads/smart_reply_output.csv")
-//            bufferedWriter!!.close()
-//            bufferedWriter = null
-//            currentLine = null
-//            smartReply!!.close()
-//            smartReply = null
-//            inputStream!!.close()
-//            inputStream = null
-//            fileReader!!.close()
-//            fileReader = null
-//            count = 1
-//            return
-//        }
+
+    fun processLine(line: String, lineCount: Long) {
 
         var currentLine = line
         val conversation = ArrayList<FirebaseTextMessage>()
@@ -189,14 +154,13 @@ class SmartReplyRunnable(lineP: String, countP: Int, handlerP: Handler?, taskCom
             FirebaseTextMessage.createForRemoteUser(
                 line.toString(), System.currentTimeMillis() - 1000, "Manoj"))
 
-//        FirebaseNaturalLanguage.getInstance().smartReply!!.
-            smartReply.suggestReplies(conversation)
+        smartReply!!.suggestReplies(conversation)
             .addOnSuccessListener { result ->
 
                 var value = currentLine
                 value = value.replace("\"", "\\\"")
                 value = "\"" + value + "\""
-                var resultText = value + ","
+                var resultText = "" + lineCount + value + ","
 
                 if (result.getStatus() == SmartReplySuggestionResult.STATUS_NOT_SUPPORTED_LANGUAGE) {
                     // The conversation's language isn't supported, so the
@@ -224,29 +188,142 @@ class SmartReplyRunnable(lineP: String, countP: Int, handlerP: Handler?, taskCom
                 updateTaskCompletion()
             }
             .addOnFailureListener {
-                updateUI("Exception: $it")
+//                Log.d(TAG, "Exception: $it")
                 updateTaskCompletion()
             }
     }
 
-
     fun updateTaskCompletion() {
-        val msg = taskCompletionHandler.obtainMessage()
-        msg.what = 2
-        taskCompletionHandler.sendMessage(msg)
+        completedTaskCount = completedTaskCount + 1
+        Utils.updatePrefWithLineNo(appContext!!, completedTaskCount)
+        updateUI("Completed: $completedTaskCount")
+        process()
     }
-    fun updateUI(message:String) {
-        if (handler == null) {
-            Log.d("MM", message)
-            val msg = taskCompletionHandler.obtainMessage()
-            msg.what = 1
-            msg.obj = message
-            taskCompletionHandler.sendMessage(msg)
-            return
-        }
-        val msg = handler!!.obtainMessage()
-        msg.what = 1
-        msg.obj = message
-        handler!!.sendMessage(msg)
+
+    fun getNextLine():String? {
+        var line: String? = fileReader!!.readLine()
+        return line
+    }
+
+
+    fun checkStatus() {
+        endTime = System.currentTimeMillis()
+        val diff = (endTime - startTime) / (1000)
+
+//        threadPoolExecutor = null
+        updateUI("Done, total $completedTaskCount records processed\nOutput: Downloads/smart_reply_output.csv\nTime: $diff(s)")
+        bufferedWriter!!.close()
+        bufferedWriter = null
+        inputStream!!.close()
+        inputStream = null
+        fileReader!!.close()
+        fileReader = null
+        smartReply!!.close()
+        smartReply = null
+        val scheduler = appContext!!.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        scheduler.cancel(SmartReplyJobService.JOB_ID)
     }
 }
+
+//
+//// Implementing the Runnable interface to implement threads.
+//class SmartReplyRunnable(lineP: String, countP: Int, handlerP: Handler?, taskCompletionHandlerP: Handler,
+//                         bufferedWriterP: BufferedWriter?, smartReplyP: FirebaseSmartReply): Runnable {
+//
+//    private var handler = handlerP
+//    private var taskCompletionHandler = taskCompletionHandlerP
+//    private var bufferedWriter = bufferedWriterP
+//    private var line = lineP
+//    private var count = countP
+//    private var smartReply: FirebaseSmartReply = smartReplyP
+//
+//    public override fun run() {
+//
+//        processLine(line)
+//
+//    }
+//
+//    fun processLine(line:String)  {
+////        if (line == null) {
+////            updateUI("Done, total $count records processed\nOutput: Downloads/smart_reply_output.csv")
+////            bufferedWriter!!.close()
+////            bufferedWriter = null
+////            currentLine = null
+////            smartReply!!.close()
+////            smartReply = null
+////            inputStream!!.close()
+////            inputStream = null
+////            fileReader!!.close()
+////            fileReader = null
+////            count = 1
+////            return
+////        }
+//
+//        var currentLine = line
+//        val conversation = ArrayList<FirebaseTextMessage>()
+//
+//        conversation.add(
+//            FirebaseTextMessage.createForRemoteUser(
+//                line.toString(), System.currentTimeMillis() - 1000, "Manoj"))
+//
+////        FirebaseNaturalLanguage.getInstance().smartReply!!.
+//            smartReply.suggestReplies(conversation)
+//            .addOnSuccessListener { result ->
+//
+//                var value = currentLine
+//                value = value.replace("\"", "\\\"")
+//                value = "\"" + value + "\""
+//                var resultText = value + ","
+//
+//                if (result.getStatus() == SmartReplySuggestionResult.STATUS_NOT_SUPPORTED_LANGUAGE) {
+//                    // The conversation's language isn't supported, so the
+//                    // the result doesn't contain any suggestions.
+//                    updateUI("Error: STATUS_NOT_SUPPORTED_LANGUAGE")
+//                } else if (result.getStatus() == SmartReplySuggestionResult.STATUS_SUCCESS) {
+//                    // Task completed successfully
+//                    for (suggestion in result.suggestions) {
+//                        var value = suggestion.text
+//                        value = value.replace("\"", "\\\"")
+//                        value = "\"" + value + "\""
+//                        resultText = resultText + value + ",";
+//                        value = suggestion.confidence.toString()
+//                        value = value.replace("\"", "\\\"")
+//                        value = "\"" + value + "\""
+//                        resultText = resultText + value + ",";
+//                    }
+//                }
+//
+//                resultText = resultText + "\n"
+//
+//                bufferedWriter!!.write(resultText)
+//                bufferedWriter!!.flush()
+//
+//                updateTaskCompletion()
+//            }
+//            .addOnFailureListener {
+//                updateUI("Exception: $it")
+//                updateTaskCompletion()
+//            }
+//    }
+//
+//
+//    fun updateTaskCompletion() {
+//        val msg = taskCompletionHandler.obtainMessage()
+//        msg.what = 2
+//        taskCompletionHandler.sendMessage(msg)
+//    }
+//    fun updateUI(message:String) {
+//        if (handler == null) {
+//            Log.d("MM", message)
+//            val msg = taskCompletionHandler.obtainMessage()
+//            msg.what = 1
+//            msg.obj = message
+//            taskCompletionHandler.sendMessage(msg)
+//            return
+//        }
+//        val msg = handler!!.obtainMessage()
+//        msg.what = 1
+//        msg.obj = message
+//        handler!!.sendMessage(msg)
+//    }
+//}
---
+++ app/src/main/java/com/manoj/smartreply/Utils.kt
@@ -0,0 +1,36 @@
+package com.manoj.smartreply
+
+import android.content.Context
+
+
+class Utils {
+    companion object {
+        val UPDATE_UI = "com.manoj.smartreply.UPDATE_UI"
+        val JOB_ID: Int = 12007
+        val prefName = "com.manoj.smartreply.pref"
+
+        fun updatePrefWithLineNo(context:Context, lineNo:Long) {
+            val editor = context.getSharedPreferences(prefName, Context.MODE_PRIVATE).edit()
+            editor.putLong("LLP", lineNo)
+            editor.apply()
+        }
+
+        fun updatePrefWithFilename(context:Context, filename:String) {
+            val editor = context.getSharedPreferences(prefName, Context.MODE_PRIVATE).edit()
+            editor.putString("filename", filename)
+            editor.apply()
+        }
+
+        fun getFilenameFromPref(context:Context): String {
+            val prefs = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            val filename = prefs.getString("filename", null)
+            return filename
+        }
+
+        fun getLLPFromPref(context:Context): Long {
+            val prefs = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            val llp = prefs.getLong("LLP", 0)
+            return llp
+        }
+    }
+}
